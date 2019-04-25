3e6cb7dbc8a4583a0a954bcf561b518eeee30f3b
Edward Barak <edward.barak@gmail.com>
2019-04-25 15:40:34 -0400

    Completed draft of main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 import peewee as pw
 from playhouse.dataset import DataSet
 from playhouse.db_url import connect
-from random import randint
+from random import randint, choice
 
 # Variables
 database='boutique'
@@ -55,72 +55,85 @@ class events(pw.Model):
     class Meta:
         database = db
 
-# Functions
-
-def create_tables(db, tables):
-    db.create_tables(tables)
-    print('Created tables {}').format(tables)
+# Functions    
 
 def generate_data(n=10):
     # rooms
-    roomTypes = ['single', 'double', 'queen', 'king']
-    rooms.create(type='single', 
-        price=200, 
-        discount=1,
-        roomPhoto='./images/single.jpg'
+    roomTypes = ['single', 'double', 'queen', 'king', 'event']
+    p = 200 # initial price
+    d = 1.0 # percentage of list price discout will reduce to
+
+    for room in roomTypes:
+        rooms.create(type=room,
+            price=p,
+            discount=d,
+            roomPhoto='./images/{}.jpg'.format(room)
         )
-    rooms.create(type='double', 
-        price=300, 
-        discount=.95,
-        roomPhoto='./images/double.jpg'
-        )
-    rooms.create(type='queen', 
-        price=400, 
-        discount=.9,
-        roomPhoto='./images/queen.jpg'
-        )
-    rooms.create(type='king', 
-        price=500, 
-        discount=.85,
-        roomPhoto='./images/king.jpg'
-        )
+        p += 100
+        d -= 0.05
 
     # customers
-    firstNames = ['John', 'Jane', 'Jim', 'Kelly']
-    lastNames = ['Doe', 'Brown', 'White', 'Black']
+    import datetime
+
+    firstNames = ['John', 'Jane', 'Jim', 'Kelly', 'Megan', 'Chris']
+    lastNames = ['Doe', 'Brown', 'White', 'Black', 'Price', 'Baker']
+    paymentTypes = ['Debit Card', 'Credit Card', 'Cash', 'Paypal']
+    
     roomNums = list(range(100, 100+n))
+    today = datetime.datetime.now()
+
     for i in range(n):
         newCustomer = {
-            'firstName': firstNames[randint(0,len(firstNames))],
-            'lastName': lastNames[randint(0,len(firstNames))],
+            'firstName': choice(firstNames),
+            'lastName': choice(lastNames),
             'customerType': 'primary',
-            'roomType': roomTypes[randint(0,len(roomTypes))],
-            'roomNum': roomNums.pop(randint(0,len(roomNums)))
-        }
+            'roomType': choice(roomTypes),
+            'roomNum': roomNums.pop(randint(0,len(roomNums))),
+            'checkIn': datetime.datetime(today.year, today.month, today.day, 8) + datetime.timedelta(days=randint(0,7)),
+            'paymentMethod': choice(paymentTypes),
+            }
+        # dependent variables
+        newCustomer['checkOut'] = newCustomer['checkIn'] + datetime.timedelta(days=randint(0,4), hours=9)
         
+        customers.create(
+            firstName=newCustomer['firstName'],
+            lastName=newCustomer['lastName'],
+            customerType=newCustomer['customerType'],
+            roomType=newCustomer['roomType'],
+            roomNum=newCustomer['roomNum'],
+            checkIn=newCustomer['checkIn'],
+            checkOut=newCustomer['checkOut'],
+            paymentMethod=newCustomer['paymentMethod'],
+            )
 
-    """
-    STOCK.create(ticker='DWDP', exchange='NYSE')
+    # events
+    eventNames = [
+        'Tinfoil Hat Convention', 
+        'Dungeon and Dragons Competition',
+        'Canadian Maple Syrup Consortium', 
+        'Make America America Again!',
+        ]
+    specialRoomReqTypes = [
+        'Quiet', 
+        'Spacious', 
+        None
+        ]
+    notes = [None, 'Customer is very particular.', 'Event could be loud.']
 
-    customers.firstName = pw.TextField()
-    customers.lastName = pw.TextField()
-    customers.customerType = pw.TextField()
-    customers.roomNum = pw.IntegerField()
-    customers.checkIn = pw.DateTimeField()
-    customers.checkOut = pw.DateTimeField()
-    customers.paymentMethod = pw.TextField()
-    customers.eventID = pw.IntegerField()
-
-    events.primaryCustomer = pw.IntegerField()
-    events.eventName = pw.TextField()
-    events.eventType = pw.TextField()
-    events.eventStart = pw.DateTimeField()
-    events.eventEnd = pw.DateTimeField()
-    events.participantCount = pw.IntegerField()
-    events.specialRoomReqs = pw.TextField()
-    events.paymentMethod = pw.TextField()
-    events.notes = pw.TextField()
-    """
+    customersTable = list(customers.select().dicts())
+    
+    for i in range(len(eventNames) - randint(1, len(eventNames) - 1)):
+        selectedCustomer = customersTable.pop(randint(0,len(customersTable)))
+        events.create(
+            primaryCustomer = selectedCustomer['id'],
+            eventName = eventNames.pop(randint(0,len(eventNames))),
+            eventStart = selectedCustomer['checkIn'],
+            eventEnd = selectedCustomer['checkOut'],
+            participantCount = randint(3,100),
+            specialRoomReqs = choice(specialRoomReqTypes),
+            paymentMethod = choice(paymentTypes),
+            notes = choice(notes),
+        )
 
 # Main
 if __name__ == "__main__":
@@ -128,19 +141,20 @@ if __name__ == "__main__":
     Database: {database}
     Host: {host}
     User:{user}
-    
-    NOTE: If you want to change these defaults you can do so on lines 7 - 9 of this file (db_funcs.py)
     """.format(database=database, host=host, user=user)
+    
     print(notice)
     
     if input('Create table schemas? (y/n)').lower() == 'y':
         # Create table schemas
-        create_tables(db, [rooms, customers, events])
+        tables = [rooms, customers, events]
+        db.drop_tables(tables)
+        print('Dropping table(s) if already in existence.')
+        db.create_tables(tables)
+        print('Created table(s).')
         
-    if input('Load data into tables? (y/n)').lower() == 'y':
-        # generate data
-        
-        # load data into tables
-        pass
+    if input('Create & load data into tables? (y/n)').lower() == 'y':
+        # generate & load data
+        generate_data()
             
     db.close()
